8327648302437af4b24309856ea03a63fd028a36
adityaBekkam <adityabekkamab@gmail.com>
2016-07-20 20:37:11 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+        data=d;
+        next=null;
+    }
+	
+}
+class Solution
+{
+
+        public static Node removeDuplicates(Node head) {
+      //Write your code here
+        if( head==null ){
+            return null ;
+        }
+        Node temp = new Node(0);
+        temp = head ;
+        while( temp.next!=null ){
+            if( temp.next.data!=temp.data ){
+                temp = temp.next ;
+            }
+            else{
+                Node n = temp.next ;
+                while( n!=null && n.data==temp.data ){
+                    n = n.next ;
+                }
+                if( n==null ){
+                    temp.next = null ;
+                    break ;
+                }
+                temp.next = n ;
+                temp = n ;
+            }
+        }
+        return head ;
+
+    }
+    
+    public static  Node insert(Node head,int data)
+    {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+    }
+    public static void display(Node head)
+        {
+              Node start=head;
+              while(start!=null)
+              {
+                  System.out.print(start.data+" ");
+                  start=start.next;
+              }
+        }
+        public static void main(String args[])
+        {
+              Scanner sc=new Scanner(System.in);
+              Node head=null;
+              int T=sc.nextInt();
+              while(T-->0){
+                  int ele=sc.nextInt();
+                  head=insert(head,ele);
+              }
+              head=removeDuplicates(head);
+              display(head);
+
+       }
+    }
